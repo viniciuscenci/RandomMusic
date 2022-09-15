664992e2a40a17a96579076825113df96ccf1423
viniciuscenci <viniciuscenci@gmail.com>
2022-09-15 15:31:48 -0300

    fix

--- music-random.2s.sh
+++ music-random.2s.sh
@@ -33,7 +33,7 @@ else
 	return "false"
 end if')"
 
-if [ isMusicOpen ]; then
+if [ isMusicOpen = "true" ]; then
   status=$(tellMusic "get player state")
 
   if [ $status == "playing" ] || [ $status == "paused" ]; then
